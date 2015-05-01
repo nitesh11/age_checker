fde1e60a7a9397a5ba46905d9cca130be5d61296
Nitesh Sethia <nitesh@qed42.com>
2015-05-01 10:55:33 +0530

    Hook_init()

--- js/age_checker.js
+++ js/age_checker.js
@@ -1,6 +1,10 @@
+var age_checker = {};
+console.log(Drupal.settings.age_checker);
 //select country
 (function($) {
   $('document').ready(function() {
+
+    console.log(Drupal.settings.age_checker);
     $('#edit-list-of-countries').change(function() {
       //alert($(this).find('option:selected').html());
       var selected_country = $(this).find('option:selected').val();
@@ -8,7 +12,6 @@
       var cookie_name = "country_selected=" + selected_country + "; path=/";
       document.cookie = cookie_name;
       location.reload();
-
     });
   });
 })(jQuery);
--- templates/age_checker.tpl.php
+++ templates/age_checker.tpl.php
@@ -6,27 +6,17 @@
  * Available variables:
  * - $age_cheker_background_img: With variable will provide background image
  * - $age_cheker_logo: This variable will provide and age gate logo
- * - $age_checker_header_massage: This variable will provide a text below logo
+ * - $age_checker_header_message: This variable will provide a text below logo
  * - $age_checker_form : This variable will provide form, which include below things
- * - list of countries: This will provide a drop down list of countries
- * - day: This will provide a day field
- * - month: This will provide a month field
- * - year: This will provide a year field
- * - remember_me: This will provide a remember me check box
- * - submit: This will provide submit button
  * - $age_checker_cookies_declaration: This variable will provide text content to describe that this site will use cookies
  * - $age_checker_drinking_statement: This variable will provide a responsible  drinking age statement
  * - $age_checker_copyright: This variable will provide a copyright statement
+ * - $footer: This variable will provide an array containing footer text and links.
  */
 print $age_cheker_background_img;
 print $age_cheker_logo;
 print $age_checker_header_message;
-print ( drupal_render($age_checker_form) );
-// print ( drupal_render($age_checker_form['day']) );
-// print ( drupal_render($age_checker_form['month']) );
-// print ( drupal_render($age_checker_form['year']) );
-// print ( drupal_render($age_checker_form['remember_me']) );
-// print ( drupal_render($age_checker_form['submit']) );
+print (drupal_render($age_checker_form));
 print $age_checker_cookies_declaration;
 print $age_checker_drinking_statement;
 print $age_checker_copyright;
