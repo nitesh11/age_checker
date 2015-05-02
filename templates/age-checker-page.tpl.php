43d2f9302166857cbf0a8baa92cf012b3d6bb8cc
Nitesh Sethia <nitesh@qed42.com>
2015-05-03 01:04:17 +0530

    Random fixes

--- templates/age-checker-page.tpl.php
+++ templates/age-checker-page.tpl.php
@@ -1,3 +1,16 @@
+<?php
+/**
+ * @file
+ * Theme implementation to display age checker on a single Drupal page.
+ *
+ * Available variables:
+ * - $age_cheker_background_img: This variable has the background image of the page.
+ * - $age_cheker_logo : This variable has the logo image of the page.
+ * - $age_checker_footer_links: This variable is an array having all the values in key value pair.
+ * - $age_checker_copyright : Copyright text for the footer of the age gate.
+ */
+?>
+
 <div id="age_checker">
   <div class="age-checker-bg">
     <?php print $age_cheker_background_img; ?>
